8a8db49704143d8f097b4d70cc1ee13034d0e798
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-04 23:52:56 +0100

    ajoute navbars

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./Auth/register/register.component";
 import {LoginComponent} from "./Auth/login/login.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 
+  { path: 'nav', component: NavbarComponent },
 
 ];
