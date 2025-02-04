7f9513db18bf5f977d53247c0ff2715e2e0d7aaf
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-05 00:52:13 +0100

    ajoute de account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {RegisterComponent} from "./Auth/register/register.component";
 import {LoginComponent} from "./Auth/login/login.component";
 import {NavbarComponent} from "./navbar/navbar.component";
+import {AccountComponent} from "./account/account.component";
 
 export const routes: Routes = [
 
@@ -9,5 +10,8 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
 
   { path: 'nav', component: NavbarComponent },
+  { path: 'account', component: AccountComponent },
+
+
 
 ];
