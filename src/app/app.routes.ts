3730fe688f96b8a7c61207e00e72d742bc2800f5
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-05 22:39:44 +0100

    ajoute home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,18 +5,23 @@ import {NavbarComponent} from "./navbar/navbar.component";
 import {AccountComponent} from "./account/account.component";
 import {FooterComponent} from "./footer/footer.component";
 import {LayoutComponent} from "./layout/layout.component";
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 
+  { path: 'home2', component: HomeComponent },
 
   {
     path: '',
     component: LayoutComponent,
     children: [
       { path: 'account', component: AccountComponent },
+
+      { path: 'home', component: HomeComponent },
+
 
     ]
   },
