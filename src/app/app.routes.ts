61b57e0f488df45265e3bfcbf9777f00983869e2
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-05 01:23:33 +0100

    ajoute layoute component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,22 @@ import {RegisterComponent} from "./Auth/register/register.component";
 import {LoginComponent} from "./Auth/login/login.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {AccountComponent} from "./account/account.component";
+import {FooterComponent} from "./footer/footer.component";
+import {LayoutComponent} from "./layout/layout.component";
 
 export const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 
-  { path: 'nav', component: NavbarComponent },
-  { path: 'account', component: AccountComponent },
 
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'account', component: AccountComponent },
 
+    ]
+  },
 
 ];
