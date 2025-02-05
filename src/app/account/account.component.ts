153a1268d15de2d033c3819dbf5ac42323022306
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2025-02-06 00:36:01 +0100

    ajoute update d account

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,15 +1,56 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from "../navbar/navbar.component";
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [
-    NavbarComponent
+  imports: [CommonModule, FormsModule
   ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit{
+  user: any = {
+    fullName: '',
+    email: '',
+    address: '',
+    phone: '',
+    birthDate: ''
+  };
+
+  userId!: number;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const authUser = JSON.parse(storedUser);
+      this.userId = authUser.id;
+
+      this.userService.getUser(this.userId).subscribe((data) => {
+        this.user = data;
+      });
+    } else {
+      alert('Utilisateur non authentifié !');
+    }
+  }
+
+  updateUser() {
+    if (!this.user.fullName || !this.user.email) {
+      alert("Veuillez remplir tous les champs obligatoires.");
+      return;
+    }
+
+    this.userService.updateUser(this.userId, this.user).subscribe(() => {
+      alert('Informations mises à jour avec succès !');
+
+      // Mettre à jour les informations de l'utilisateur dans le localStorage
+      const updatedUser = { ...this.user, id: this.userId }; // Ajoutez l'ID si nécessaire
+      localStorage.setItem('user', JSON.stringify(updatedUser));
+    });
+  }
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUser(userId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${userId}`);
+  }
+
+  updateUser(userId: number, user: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${userId}`, user);
+  }
+}
